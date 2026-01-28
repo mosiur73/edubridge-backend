f5edb5144870d11d3b177bc818df47685ec28976
mosiur73 <mosiurrahman.cmt173@gmail.com>
2026-01-28 21:35:05 +0600

    setup availability routes

--- src/modules/tutor/tutor.routes.ts
+++ src/modules/tutor/tutor.routes.ts
@@ -1,18 +1,18 @@
-import { Router } from "express";
-import auth, { UserRole } from "../../middleware/auth";
-import { tutorController } from "./tutor.controller";
+// import { Router } from "express";
+// import auth, { UserRole } from "../../middleware/auth";
+// import { tutorController } from "./tutor.controller";
 
 
-const router = Router();
+// const router = Router();
 
-// Public routes
-router.post("/",auth(UserRole.TUTOR), tutorController.createTutorProfile)
-router.get("/", tutorController.getAllTutors);           
-router.get("/:id", tutorController.getTutorById);       
+// // Public routes
+// router.post("/",auth(UserRole.TUTOR), tutorController.createTutorProfile)
+// router.get("/", tutorController.getAllTutors);           
+// router.get("/:id", tutorController.getTutorById);       
 
-// Tutor private routes
-router.put("/profile", auth(UserRole.TUTOR), tutorController.updateTutorProfile);          // Update profile
-router.put("/availability", auth(UserRole.TUTOR), tutorController.updateAvailability);     // Update availability
-router.get("/bookings", auth(UserRole.TUTOR), tutorController.getTutorBookings);          // Tutor bookings
+// // Tutor private routes
+// router.put("/profile", auth(UserRole.TUTOR), tutorController.updateTutorProfile);          // Update profile
+// router.put("/availability", auth(UserRole.TUTOR), tutorController.updateAvailability);     // Update availability
+// router.get("/bookings", auth(UserRole.TUTOR), tutorController.getTutorBookings);          // Tutor bookings
 
-export const tutorRouter:Router= router;
+// export const tutorRouter:Router= router;
